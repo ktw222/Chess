dcf2abd2639f33d062defed97aa7c65bd1096a77
ktw222 <kw855@byu.edu>
2024-02-22 17:33:56 -0700

    making Chess root file

--- server/src/main/java/dataAccess/MemoryAuthDAO.java
+++ server/src/main/java/dataAccess/MemoryAuthDAO.java
@@ -2,9 +2,18 @@ package dataAccess;
 
 import model.UserData;
 
+import java.util.ArrayList;
+
+
 public class MemoryAuthDAO implements AuthDAO {
-    private getUser(UserData) {
-        return UserData;
+    final private ArrayList<UserData> users = new ArrayList<>();
+    public UserData getUser(UserData user) {
+        return user;
+    }
+    public UserData createUser(UserData user) {
+        user = new UserData(user.username(), user.password(), user.email());
+        users.add(user);
+        return user;
     }
 
 }
