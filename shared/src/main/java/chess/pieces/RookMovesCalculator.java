15ae1aac5f0558a8072d38e755a7d14d74b8d3ae
ktw222 <kw855@byu.edu>
2024-02-29 19:34:48 -0700

    making Chess root file

--- shared/src/main/java/chess/pieces/RookMovesCalculator.java
+++ shared/src/main/java/chess/pieces/RookMovesCalculator.java
@@ -10,69 +10,32 @@ public class RookMovesCalculator {
     public ArrayList<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessMove> possibleMoves = new ArrayList<>();
         //moveVerticallyUp (only rows change)
-        int col = myPosition.getColumn();
-        for (int row = (myPosition.getRow() +1); row <= 8; row++) {
-            ChessPosition movePosition;
-            movePosition = new ChessPosition(row,col);
-            ChessMove actualMove = new ChessMove(myPosition, movePosition);
-            if(board.getPiece(movePosition) == null){
-                possibleMoves.add(actualMove);
-            }
-            else {
-                if (board.getPiece(movePosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                    possibleMoves.add(actualMove);
-                }
-                break;
-            }
-        }
-        //moveVerticallyDown
-        for (int row = (myPosition.getRow() -1); row > 0; row--) {
-            ChessPosition movePosition;
-            movePosition = new ChessPosition(row,col);
-            ChessMove actualMove = new ChessMove(myPosition, movePosition);
-            if(board.getPiece(movePosition) == null){
-                possibleMoves.add(actualMove);
-            }
-            else {
-                if (board.getPiece(movePosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                    possibleMoves.add(actualMove);
-                }
-                break;
-            }
-        }
-        //moveHorizontallyRight
+        possibleMoves.addAll(calcRookMoves(board, myPosition, 1, 0)); // Up
+        possibleMoves.addAll(calcRookMoves(board, myPosition, -1, 0)); // Down
+        possibleMoves.addAll(calcRookMoves(board, myPosition, 0, 1)); // Right
+        possibleMoves.addAll(calcRookMoves(board, myPosition, 0, -1)); // Left
+        return possibleMoves;
+    }
+    public ArrayList<ChessMove> calcRookMoves(ChessBoard board, ChessPosition myPosition, int rowIncrement, int colIncrement) {
+        ArrayList<ChessMove> possibleMoves = new ArrayList<>();
         int row = myPosition.getRow();
-        for (int loopCol = (myPosition.getColumn() +1); loopCol <= 8; loopCol++) {
+        int col = myPosition.getColumn();
 
-            ChessPosition movePosition;
-            movePosition = new ChessPosition(row,loopCol);
+        while (row + rowIncrement >= 1 && row + rowIncrement <= 8 && col + colIncrement >= 1 && col + colIncrement <= 8) {
+            row += rowIncrement;
+            col += colIncrement;
+            ChessPosition movePosition = new ChessPosition(row, col);
             ChessMove actualMove = new ChessMove(myPosition, movePosition);
-            if(board.getPiece(movePosition) == null){
+            if (board.getPiece(movePosition) == null) {
                 possibleMoves.add(actualMove);
-            }
-            else {
+            } else {
                 if (board.getPiece(movePosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
                     possibleMoves.add(actualMove);
                 }
                 break;
             }
         }
-        //moveHorizontallyLeft
-        for (int loopCol = (myPosition.getColumn() -1); loopCol >0; loopCol--) {
 
-            ChessPosition movePosition;
-            movePosition = new ChessPosition(row,loopCol);
-            ChessMove actualMove = new ChessMove(myPosition, movePosition);
-            if(board.getPiece(movePosition) == null){
-                possibleMoves.add(actualMove);
-            }
-            else {
-                if (board.getPiece(movePosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                    possibleMoves.add(actualMove);
-                }
-                break;
-            }
-        }
-        return possibleMoves; //placeholder
+        return possibleMoves;
     }
 }
