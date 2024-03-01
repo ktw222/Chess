9d8a1341131e545276619cda8e36e2a74f439eaf
ktw222 <kw855@byu.edu>
2024-02-29 19:28:55 -0700

    making Chess root file

--- shared/src/main/java/chess/pieces/BishopMovesCaclulator.java
+++ shared/src/main/java/chess/pieces/BishopMovesCaclulator.java
@@ -8,81 +8,29 @@ public class BishopMovesCaclulator {
     public ArrayList<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition) {
 
         ArrayList<ChessMove> possibleMoves = new ArrayList<>();
-        ChessPosition currPosition;
-        currPosition = myPosition;
-        int col = myPosition.getColumn()+1;
-        for(int row = (myPosition.getRow()+1) ; row<=8; row++) {
-                    if(col <= 8){
-                        ChessPosition movePosition;
-                        movePosition = new ChessPosition(row,col);
-                        ChessMove actualMove = new ChessMove(myPosition, movePosition);
-                        if(board.getPiece(movePosition) == null){
-                            possibleMoves.add(actualMove);
-                            col = col+1;
-                        }
-                        else {
-                            if (board.getPiece(movePosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                                possibleMoves.add(actualMove);
-                            }
-                            break;
-                        }
-                    }
-        }
-        col = myPosition.getColumn()+1;
-        for(int row = (currPosition.getRow()-1); row>0; row--) {
-            if(col <= 8){
-                ChessPosition movePosition;
-                movePosition = new ChessPosition(row, col);
-                ChessMove actualMove = new ChessMove(myPosition, movePosition);
-                if(board.getPiece(movePosition) == null){
+
+        possibleMoves.addAll(calcMultMoves(board, myPosition, 1, 1));
+        possibleMoves.addAll(calcMultMoves(board, myPosition, 1, -1));
+        possibleMoves.addAll(calcMultMoves(board, myPosition, -1, 1));
+        possibleMoves.addAll(calcMultMoves(board, myPosition, -1, -1));
+        return possibleMoves;
+    }
+    private  ArrayList<ChessMove> calcMultMoves(ChessBoard board, ChessPosition myPosition, int incrRow, int incrCol) {
+        ArrayList<ChessMove> possibleMoves = new ArrayList<>();
+        int col = myPosition.getColumn() + incrCol;
+        for (int row = myPosition.getRow() + incrRow; row >= 1 && row <= 8 && col >= 1 && col <= 8; row += incrRow, col += incrCol) {
+            ChessPosition movePosition = new ChessPosition(row, col);
+            ChessPiece piece = board.getPiece(movePosition);
+            ChessMove actualMove = new ChessMove(myPosition, movePosition);
+            if (piece == null) {
+                possibleMoves.add(actualMove);
+            } else {
+                if (piece.getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
                     possibleMoves.add(actualMove);
-                    col = col +1;
                 }
-                else{
-                    if(board.getPiece(movePosition).getTeamColor() != board.getPiece(currPosition).getTeamColor()) {
-                        possibleMoves.add(actualMove);
-                    }
-                    break;
-                }
-            }
-        }
-        col = myPosition.getColumn()-1;
-        for(int row = (currPosition.getRow()+1) ; row<=8; row++) {
-            if (col>0) {
-                ChessPosition movePosition;
-                movePosition = new ChessPosition(row,col);
-                ChessMove actualMove = new ChessMove(myPosition, movePosition);
-                if(board.getPiece(movePosition) == null){
-                    possibleMoves.add(actualMove);
-                    col--;
-                }
-                else{
-                    if(board.getPiece(movePosition).getTeamColor() != board.getPiece(currPosition).getTeamColor()) {
-                        possibleMoves.add(actualMove);
-                    }
-                    break;
-                }
-            }
-        }
-        col = myPosition.getColumn()-1;
-        for(int row = (currPosition.getRow()-1) ; row>0; row--) {
-            if(col >0){
-                ChessPosition movePosition;
-                movePosition = new ChessPosition(row,col);
-                ChessMove actualMove = new ChessMove(myPosition, movePosition);
-                if(board.getPiece(movePosition) == null){
-                    possibleMoves.add(actualMove);
-                    col--;
-                }
-                else{
-                    if(board.getPiece(movePosition).getTeamColor() != board.getPiece(currPosition).getTeamColor()) {
-                        possibleMoves.add(actualMove);
-                    }
-                    break;
-                }
+                break;
             }
         }
         return possibleMoves;
     }
-
 }
