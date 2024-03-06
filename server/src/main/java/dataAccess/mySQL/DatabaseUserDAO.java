14af65b575b08b6cb3882d93051b9c303c3d0956
ktw222 <kw855@byu.edu>
2024-03-06 16:28:15 -0700

    making Chess root file

--- server/src/main/java/dataAccess/mySQL/DatabaseUserDAO.java
+++ server/src/main/java/dataAccess/mySQL/DatabaseUserDAO.java
@@ -12,8 +12,11 @@ import static java.sql.Statement.RETURN_GENERATED_KEYS;
 import static java.sql.Types.NULL;
 
 public class DatabaseUserDAO implements AuthDAO {
+    public DatabaseUserDAO() throws DataAccessException {
+        configureDatabase();
+    }
     public UserData createUser(UserData user) throws DataAccessException {
-        var statement = "INSERT INTO pet (name, type, json) VALUES (?, ?, ?)";
+        var statement = "INSERT INTO user (name, type, json) VALUES (?, ?, ?)";
         var json = new Gson().toJson(user);
         var id = executeUpdate(statement, user, json);
         return new UserData(user.username(), user.password(), user.email());
@@ -40,5 +43,60 @@ public class DatabaseUserDAO implements AuthDAO {
         var user = new Gson().fromJson(json, UserData.class);
         return user;
     }
+    public void clearUsers() throws DataAccessException {
+        var statement = "TRUNCATE user";
+        executeUpdate(statement);
+    }
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var db = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) db.setString(i + 1, p);
+                    else if (param instanceof Integer p) db.setInt(i + 1, p);
+                    else if (param instanceof PetType p) db.setString(i + 1, p.toString());
+                    else if (param == null) db.setNull(i + 1, NULL);
+                }
+                db.executeUpdate();
+
+                var rs = db.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("unable to update database");
+        }
+    }
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  user (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`username`),
+              INDEX(password),
+              INDEX(email)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+
+    private void configureDatabase() throws DataAccessException{
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Unable to configure database");
+        }
+    }
+}
 
 }
